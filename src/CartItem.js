99ce007b02a3f9e4ba68c0e262d3df0e256a98c1
ankitverma132 <ankitv.ashokaec19@gmail.com>
2020-09-19 17:10:20 +0530

    Increase quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,22 @@ class CartItem extends React.Component{
             qty : 1,
             img : ''
         }
+        //Another way
+        this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    // increaseQuantity(){
+    //     //console.log('Test');
+    //     // It will throw an error because this is undefined here
+    //     //console.log('this.state',this.state);
+    //     console.log('this.state', this.state);
+    // }
+    //Another way using arrow function
+    //It will bind value of this to instant of CartItem
+    increaseQuantity = () =>{
+        //console.log('Test');
+        // It will throw an error because this is undefined here
+        //console.log('this.state',this.state);
+        console.log('this.state', this.state);
     }
     render(){
         // Using object destructuring
@@ -29,9 +45,15 @@ class CartItem extends React.Component{
                     <div className = "cart-item-actions">
                         {/* Buttons */}
                         {/* Use image from flaticon.com */}
-                        <img alt = "increase" className = "action-icons" src = "https://www.flaticon.com/premium-icon/icons/svg/2985/2985071.svg" />
-                        <img alt = "decrease" className = "action-icons" src = "https://www.flaticon.com/premium-icon/icons/svg/2985/2985073.svg" />
-                        <img alt = "delete" className = "action-icons" src = "https://www.flaticon.com/premium-icon/icons/svg/2907/2907762.svg" />
+                        <img alt = "increase" className = "action-icons"
+                         src = "https://www.flaticon.com/premium-icon/icons/svg/2985/2985071.svg"
+                        //onClick = {this.increaseQuantity.bind(this)}
+                        onClick = {this.increaseQuantity}
+                        />
+                        <img alt = "decrease" className = "action-icons" 
+                        src = "https://www.flaticon.com/premium-icon/icons/svg/2985/2985073.svg" />
+                        <img alt = "delete" className = "action-icons" 
+                        src = "https://www.flaticon.com/premium-icon/icons/svg/2907/2907762.svg" />
                     </div>
                 </div>
             </div>
